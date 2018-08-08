9599f01e4f5a1e271785d51b3b9118cf2d78ada8
chen <17600105737@163.com>
2018-08-08 20:37:32 +0800

    完成英雄添加

--- src/routes/router.js
+++ src/routes/router.js
@@ -8,11 +8,13 @@ Vue.use(VueRouter);
 import HeroList from '../views/hero/List.vue';
 import WeaponList from '../views/weapon/List.vue';
 import EquipList from '../views/Equip/List.vue';
+import HeroAdd from '../views/hero/Add.vue';
 var router = new VueRouter({
     linkActiveClass:'active',
     routes: [
         {path:'/',redirect:'/hero'},
         {name:'hero',path:'/hero',component:HeroList},
+        {name:'heroAdd',path:'/hero/add',component:HeroAdd},
         {name:'weapon',path:'/weapon',component:WeaponList},
         {name:'equip',path:'/equip',component:EquipList}
     ]
