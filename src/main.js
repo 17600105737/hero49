122372870b0528dd0daf8e0338d8bb0a239b846d
chen <17600105737@163.com>
2018-08-08 17:40:45 +0800

    渲染主页

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 引入全局样式文件
+import 'bootstrap/dist/css/bootstrap.css';
+import './assets/css/index.css';
+
 new Vue({
   el: '#app',
   render: h => h(App)
